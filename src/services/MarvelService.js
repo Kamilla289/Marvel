c97cd89870080be63c5ffbaa7c69377dbd833585
Kamilla289 <kami.rivay56@gmail.com>
2025-01-22 22:56:50 +0500

    Added truncation for character description and handling of missing data

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -12,13 +12,31 @@ class MarvelService {
   }
 
   getAllCharacters = async () => {
-    return this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    return res.data.results.map(this._transformCharacter);
   }
 
   getCharacter = async (id) => {
-    return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    return this._transformCharacter(res.data.results[0]);
   }
 
+  _transformCharacter = (char) => {
+    const maxDescriptionLength = 200;
+    let description = char.description || "Данных о персонаже нет";
+
+    if (description.length > maxDescriptionLength) {
+      description = description.substring(0, maxDescriptionLength) + '...';
+    }
+
+    return {
+      name: char.name,
+      description: description,
+      thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+      homepage: char.urls[0].url,
+      wiki: char.urls[1].url
+    }
+  }
 }
 
 export default MarvelService;
