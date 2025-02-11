b8a596d8b2d85a739cac4dacfcd35189eb8c50cd
Kamilla289 <kami.rivay56@gmail.com>
2025-02-11 20:02:25 +0500

    fix: component

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import PropTypes from 'prop-types';
 import './charInfo.scss';
 import Spinner from "../Spinner/Spinner";
 import ErrorMessage from "../errorMessage/ErrorMessage";
@@ -115,6 +116,10 @@ const View = ({ char }) => {
             </ul>
         </>
     )
+}
+
+CharInfo.propTypes = {
+    charId: PropTypes.string
 }
 
 export default CharInfo;
