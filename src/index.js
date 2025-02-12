06e58ef60838365e340780d5c19374c796c96522
Kamilla289 <kami.rivay56@gmail.com>
2025-02-12 19:54:57 +0500

    fix: routes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from "react-dom/client";
 import App from "./components/app/App";
 import "./style/style.scss";
 
-ReactDOM
-  .createRoot(document.getElementById("root"))
-  .render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
