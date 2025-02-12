d7fcb9c516482cf7193b895de2dcb12eb59ad428
Kamilla289 <kami.rivay56@gmail.com>
2025-02-12 19:30:11 +0500

    update version 19

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-import { createRoot } from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import App from "./components/app/App";
 import "./style/style.scss";
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<App />);
+
+ReactDOM
+  .createRoot(document.getElementById("root"))
+  .render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
